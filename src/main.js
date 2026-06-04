f0694c509baf9603b0d2f4bf9ec770f92e157b72
huijinsim <shjiou123@gmail.com>
2026-06-04 17:13:04 +0900

    forest_project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import './style.css'
+import { initForest } from './forest.js'
 
 const introPage = document.getElementById('intro-page')
 const mainPage = document.getElementById('main-page')
 const enterButton = document.getElementById('enter-main')
+const forestCanvas = document.getElementById('forest-canvas')
+
+let forestStarted = false
 
 enterButton.addEventListener('click', () => {
   introPage.classList.add('is-hidden')
   mainPage.classList.add('is-visible')
   mainPage.setAttribute('aria-hidden', 'false')
+
+  if (!forestStarted) {
+    forestStarted = true
+    initForest(forestCanvas)
+  }
 })
